59c20b313a1db5f673c1fd623fff1fdc204f7480
dhilst <danielhilst@gmail.com>
2019-11-04 20:56:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+
+# The text of the README file
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="delegate",
     author="Daniel Hilst Selli",
     author_email="danielhilst@gmail.com",
     version="1.0",
-    py_modules="delegate",
+    py_modules=["delegate"],
     python_requires=">=3",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="Apache-2.0",
+
 )
